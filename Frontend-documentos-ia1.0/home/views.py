a1619fce28514a83ce5b4750082bcf7695e2ea68
Andres <oquendofredy48@gmail.com>
2026-05-13 11:15:03 -0500

    cambios 0

--- Frontend-documentos-ia1.0/home/views.py
+++ Frontend-documentos-ia1.0/home/views.py
@@ -15,7 +15,7 @@ def home_view(request):
 # Registro de usuarios
 def register_view(request):
     if request.method == "POST":
-        nombre = request.POST.get("nombre")
+        nombre = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("password")
         password2 = request.POST.get("password2")
