66089f4180dddb8db6c7341751632fdca736bda1
Andres <oquendofredy48@gmail.com>
2026-06-25 08:33:11 -0500

    porfa funciona

--- Frontend-documentos-ia1.0/docval/settings.py
+++ Frontend-documentos-ia1.0/docval/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -36,7 +37,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True 
 
 ROOT_URLCONF = 'docval.urls'
 
